5fabf3c05d7e5d15f9a081ab041db52d1786ad33
Shai_Base <shaibase1328@gmail.com>
2024-01-28 03:54:21 +0800

    added printing of results

--- Check the first and last number.py
+++ Check the first and last number.py
@@ -8,8 +8,13 @@ def check(number_list):
     first_number = number_list[0]
     last_number = number_list [4]
 
-    if first_number == last_number
+    if first_number == last_number:
         return True
     else:
         return False
-    
+
+first_list = [10, 20, 30, 40, 10]
+print('The result is:', check(first_list))
+
+second_list = [75, 65, 35, 75, 30]
+print('The result is:', check(second_list))
