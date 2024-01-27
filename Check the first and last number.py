c3faf8ce58e5fee0925fc995b825f48e8f021b70
Shai_Base <shaibase1328@gmail.com>
2024-01-28 03:44:42 +0800

    made a function

---
+++ Check the first and last number.py
@@ -0,0 +1,11 @@
+# Write a function to return True
+# if the first and last number of a given list is same.
+# If numbers are different then return False.
+
+def check(number_list):
+    print('Given List:', number_list)
+
+    first_number = number_list[0]
+    last_number = number_list [4]
+
+    
